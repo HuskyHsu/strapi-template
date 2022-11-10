84b96d59209c4e3e0533f116f3b9e9390ae50453
huskyHsu <schi.hsu@gmail.com>
2022-11-10 20:10:43 +0800

    feat: 🎸 test depoly

--- config/database.ts
+++ config/database.ts
@@ -1,12 +1,15 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5433),
-      database: env("DATABASE_NAME", "strdb"),
-      user: env("DATABASE_USERNAME", "user"),
-      password: env("DATABASE_PASSWORD", "password"),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
